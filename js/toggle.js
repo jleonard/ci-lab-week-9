0cc0a4fc48df706ddd07c5c20a91b03a3401bdf0
John Leonard <thejohnleonard@gmail.com>
2015-03-22 20:05:23 -0400

    bp wip

--- js/toggle.js
+++ js/toggle.js
@@ -1,4 +1,7 @@
 var elements = document.querySelectorAll('.toggle');
 Array.prototype.forEach.call(elements, function(element, index){
-  el.parentNode.classList.toggle('active');
+  element.addEventListener('click',function(e){
+    console.log('toggle');
+    element.parentNode.classList.toggle('active');
+  });
 });
